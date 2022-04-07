b2303c5650a86fddf7efe933ee487c9b33473fbb
Merritske <merritske@gmail.com>
2022-04-07 12:58:38 +0200

    update 7

---
+++ tuto6/index.js
@@ -0,0 +1,28 @@
+ var fs = require('fs')
+
+
+ fs.readFile('./combolist.txt', 'utf-8', (err, res)=>{
+  let arrayList = res.split("\r\n");
+ //console.log(arrayList)
+  arrayList.map((user)=>{
+   
+  fs.appendFile('./output.txt', `${user.split("\:")[0]}\n`
+,'utf-8', (err)=>{
+    console.log("saved")
+}) }) 
+ })
+//   
+
+//  console.log(allUser)
+// 
+
+
+// var LineByLineReader = require('line-by-line'), //geimporteerde package
+//     lr = new LineByLineReader('combolist.txt'); //datasource
+
+//     lr.on('line', function(line){
+//         console.log(line.split(":")[0])
+// fs.appendFile('output.txt', `${line.split(":")[0]}\n`, "utf-8", (err)=>{
+//     console.log("saved")
+// })
+//  

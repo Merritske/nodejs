1780c46e0352ecf58dd02b9cf5c48ed1099bc7c2
merritske <merritske@gmail.com>
2022-04-13 16:04:26 +0200

    spielerei

---
+++ Oefening/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const http = require('http')
+
+  
+
+    fs.readFile("curriculum.json", "utf-8",(err, result)=>{
+let data = JSON.stringify(result, null, 2)
+let arrayList = data.split("\\r\\n");
+// console.log(arrayList)
+ http.createServer(function(req, res){
+    res.writeHead(200, {"ContenType": "text/html"})
+  arrayList.map((user)=>{
+   let m = user.split("\\")
+
+  if(m.length >= 4){
+
+   let line = m.slice(1,m.length)
+ res.write(`<p>${line}</p>`)
+   
+ }
+
+})
+  res.end()
+ }).listen(8080, function(){
+     console.log("server is running at port 8080")
+ })
+
+  
+
+
+
+       
+ 
+
+
+})
---
+++ Oefening/readme.txt
@@ -0,0 +1,13 @@
+{"persona":{
+    "naam": "An Verbeeck",
+    "telefoon": "+32472694102",
+    "email": "merritske@gmail.com"
+},
+"competenties":
+"leergierig, creatief, betrouwbaar, kan goed luisteren",
+"kennis": "verschillende talen, grafische computerprogramma's, front-end programma's en een basis van back-end",
+"werkervaring": "onderwijs, administratie, verkoop",
+"diploma's": "master in de productdesign",
+"getuigschriften": "front-end development, digital coordinator",
+"hobbies": "muziek, literatuur, natuur"
+}

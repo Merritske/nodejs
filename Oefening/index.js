949e4e489eb511048af2dcbc9b0ef23a4f653429
merritske <merritske@gmail.com>
2022-04-14 11:23:09 +0200

    update 14

--- Oefening/index.js
+++ Oefening/index.js
@@ -1,31 +1,32 @@
 #!/usr/bin/env node
+console.log("Bel of mail naar An Verbeeck om haar een job aan te bieden; 0472694102; merritske@gmail.com")
 
-const fs = require('fs')
-const http = require('http')
+// const fs = require('fs')
+// const http = require('http')
 
   
 
-    fs.readFile("curriculum.json", "utf-8",(err, result)=>{
-let data = JSON.stringify(result, null, 2)
-let arrayList = data.split("\\r\\n");
-// console.log(arrayList)
- http.createServer(function(req, res){
-    res.writeHead(200, {"ContenType": "text/html"})
-  arrayList.map((user)=>{
-   let m = user.split("\\")
+//     fs.readFile("curriculum.json", "utf-8",(err, result)=>{
+// let data = JSON.stringify(result, null, 2)
+// let arrayList = data.split("\\r\\n");
+// // console.log(arrayList)
+//  http.createServer(function(req, res){
+//     res.writeHead(200, {"ContenType": "text/html"})
+//   arrayList.map((user)=>{
+//    let m = user.split("\\")
 
-  if(m.length >= 4){
+//   if(m.length >= 4){
 
-   let line = m.slice(1,m.length)
- res.write(`<p>${line}</p>`)
+//    let line = m.slice(1,m.length)
+//  res.write(`<p>${line}</p>`)
    
- }
+//  }
 
-})
-  res.end()
- }).listen(8080, function(){
-     console.log("server is running at port 8080")
- })
+// })
+//   res.end()
+//  }).listen(8080, function(){
+//      console.log("server is running at port 8080")
+//  })
 
   
 
@@ -35,4 +36,4 @@ let arrayList = data.split("\\r\\n");
  
 
 
-})
+// })
